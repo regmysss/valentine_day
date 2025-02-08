5ef3c56d56060d8becd74c68631d963a33e9194c
regmysss <kabanigor3@gmail.com>
2025-02-08 19:08:17 +0200

    feat: update heart image and enhance text animation functionality

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ const text = [
     'Нехай цей день принесе тобі усмішку, нові можливості та приємні моменти. Почни його з позитивних думок і гарного настрою!',
     'Ти заслуговуєш на найкраще. Твоя доброта та посмішка роблять цей світ яскравішим, тому цінуй себе і свої маленькі перемоги кожного дня.',
     'Кожен день унікальний, і він не повториться. Тож цінуй його і наповнюй любов\'ю та гарними емоціями.',
-    '<img class="heart" src="../valentine_day/source/images/heart.png" alt="heart" />'
+    '<img class="heart" src="../valentine_day/source/images/heart_2k.png" alt="heart" />'
 ]
 
 const titleElement = document.querySelector('.title');
@@ -21,12 +21,30 @@ let step = 0;
 
 function changeText() {
     titleElement.innerHTML = title[step];
-    textElement.innerHTML = text[step];
+    if (step === title.length - 1)
+        textElement.innerHTML = text[step];
+    else
+        typeText(text[step], step);
 };
+
+function typeText(str, stepParam, position = 0) {
+    if (position === str.length || step != stepParam) return;
+
+    textElement.innerHTML += str[position];
+    position++;
+
+    const interval = getRandInterval(25, 60);
+    setTimeout(() => typeText(str, stepParam, position), interval);
+}
+
+function getRandInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 btnNextElement.addEventListener('click', () => {
     if (step < title.length - 1) {
         step++;
+        textElement.innerHTML = '';
         changeText();
         btnPrevElement.style.display = 'block';
 
@@ -38,6 +56,7 @@ btnNextElement.addEventListener('click', () => {
 btnPrevElement.addEventListener('click', () => {
     if (step > 0) {
         step--;
+        textElement.innerHTML = '';
         changeText();
 
         if (step === 0)
